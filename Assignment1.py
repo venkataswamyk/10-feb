73c9b1cb71b7c4dbd1b1c56b2745007bf9bfbf6c
venkataswamyk <99276480+venkataswamyk@users.noreply.github.com>
2022-02-10 21:56:12 +0530

    done assignment1

---
+++ Assignment1.py
@@ -0,0 +1,12 @@
+sno=int(input("enter the student number:"))
+sname=input("enter the student name:")
+sgroup=input("enter the student group:")
+s1=int(input("maths marks:"))
+s2=int(input("physics marks:"))
+s3=int(input("computer science"))
+s1>=35and
+s2>=35and
+s3>=35
+print("pass")
+else:
+print("fail")
